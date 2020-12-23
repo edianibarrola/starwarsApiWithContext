470b68ffeae31ed9fba6ffb9cfb03398d31c4dcb
edianibarrola <edianibarrola@gmail.com>
2020-12-23 17:17:08 +0000

    update

--- src/js/component/PlanetCard.js
+++ src/js/component/PlanetCard.js
@@ -24,7 +24,25 @@ export class PlanetCard extends React.Component {
 			})
 			.then(jsonifiedResponse => {
 				console.log("RESULT", jsonifiedResponse.result.properties);
-				this.setState({ planetInfo: jsonifiedResponse.result.properties });
+				this.setState({
+					planetInfo: jsonifiedResponse.result.properties,
+					key0: "Name: ",
+					key1: "Diameter: ",
+					key2: "Orbital Period: ",
+					key3: "Rotation Period: ",
+					key4: "Gravity: ",
+					key5: "Climate: ",
+					key6: "Terrain: ",
+					val0: jsonifiedResponse.result.properties.name,
+					val1: jsonifiedResponse.result.properties.diameter,
+					val2: jsonifiedResponse.result.properties.orbital_period,
+					val3: jsonifiedResponse.result.properties.rotation_period,
+					val4: jsonifiedResponse.result.properties.gravity,
+					val5: jsonifiedResponse.result.properties.climate,
+					val6: jsonifiedResponse.result.properties.terrain,
+					image:
+						"https://b36ca17f-b693-4c92-b67d-94fa519fcf26.ws-us03.gitpod.io/files/download/?id=091b25f0-6f39-454f-addc-52c2c7b601ec"
+				});
 			})
 			.catch(function(error) {
 				console.log("Looks like there was a problem: \n", error);
@@ -42,8 +60,7 @@ export class PlanetCard extends React.Component {
 								<h4 className="card-title">{this.state.planetInfo.name}</h4>
 								<li> Population: </li>
 								<li> Terrain: </li>
-								<Link
-									to={{ pathname: "/BigPlanetCard/" + this.props.propPlanetUid, state: this.state }}>
+								<Link to={{ pathname: "/detailsview/" + this.props.propPlanetUid, state: this.state }}>
 									<button className="btn btn-secondary" />
 								</Link>
 								<p className="bottomTitleSmall align-content-bottom float-left">Databank</p>
