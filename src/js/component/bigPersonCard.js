060b3973424f3c40eacfbca83453e001e85f13a2
edianibarrola <edianibarrola@gmail.com>
2020-12-23 13:15:58 +0000

    update

--- src/js/component/bigPersonCard.js
+++ src/js/component/bigPersonCard.js
@@ -3,6 +3,7 @@ import swTopImage from "/workspace/react-hello-webapp/src/img/starwarsposter.jpg
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import swSideImage from "/workspace/react-hello-webapp/src/img/starwarsHorizontal.jpg";
+import logoIMG from "/workspace/react-hello-webapp/src/img/swLogo.png";
 
 export class BigPersonCard extends React.Component {
 	constructor() {
@@ -14,32 +15,43 @@ export class BigPersonCard extends React.Component {
 	render() {
 		let person = this.props.location.state.personInfo ? this.props.location.state.personInfo : null;
 		return (
-			<div className="mx-auto" style={{ maxWidth: "80vw", maxHeight: "80vh " }}>
-				<div className="card cardBig mb-3 mx-auto" style={{ maxWidth: "auto", maxHeight: "auto " }}>
-					<div className="row no-gutters">
-						<div className="col-md-4">
-							<img src={swSideImage} className="card-img cardImgBig" alt="..." />
-						</div>
-						<div className="col-md-8 cardBgBig">
-							<div className="card-body pt-2 text-center card-bodyBig">
-								<h2 className="card-title mx-auto">{person.name}</h2>
-								<p className="card-text  ">
-									<ul className="mx-auto col-3">
-										<li>Height: {person.height}</li>
-										<li>Mass (on Earth): {person.mass}</li>
-										<li>Hair Color: {person.hair_color}</li>
-										<li>Skin Color: {person.skin_color}</li>
-										<li>Eye Color: {person.eye_color}</li>
-										<li>Birth Year: {person.birth_year}</li>
-										<li>Gender: {person.gender}</li>
-									</ul>
-								</p>
+			<>
+				<img className="mx-auto" src={logoIMG} />
+				<div className="mx-auto" style={{ maxWidth: "80vw", maxHeight: "80vh " }}>
+					<div className="card cardBig mb-3 mx-auto" style={{ maxWidth: "auto", maxHeight: "auto " }}>
+						<div className="row no-gutters">
+							<div className="col-md-4">
+								<img src={swSideImage} className="card-img cardImgBig" alt="..." />
 							</div>
-							<div className="d-flex align-content-bottom float-left  bottomTitleBig">Databank</div>
+							<div className="col-md-8 cardBgBig">
+								<div className="card-body pt-2 text-center card-bodyBig">
+									<h2 className="card-title mx-auto">{person.name}</h2>
+									<p className="card-text d-flex ">
+										<ul className="mx-auto col-3">
+											<li>Height: {person.height}</li>
+											<li>Mass (on Earth): {person.mass}</li>
+											<li>Hair Color: {person.hair_color}</li>
+											<li>Skin Color: {person.skin_color}</li>
+											<li>Eye Color: {person.eye_color}</li>
+											<li>Birth Year: {person.birth_year}</li>
+											<li>Gender: {person.gender}</li>
+										</ul>
+										<p className="text-left px-2">
+											Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+											tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+											quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+											consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+											cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+											non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+										</p>
+									</p>
+								</div>
+								<div className="d-flex align-content-bottom float-left  bottomTitleBig">Databank</div>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
+			</>
 		);
 	}
 }
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logoIMG from "/workspace/react-hello-webapp/src/img/swLogo.png";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar navbar-light bg-light mb-3 justify-content-between">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">Home</span>
 			</Link>
+
 			<div className="ml-auto">
 				<Link to="/person">
 					<button className="btn btn-primary">Check the Context in action</button>
